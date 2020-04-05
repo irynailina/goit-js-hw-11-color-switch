0b4ef54d1c98638378c3ded00acfc9bd77476bd2
irynailina <irynailina@gmail.com>
2020-04-05 19:02:50 +0300

    color-switch created

---
+++ index.js
@@ -0,0 +1,44 @@
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const colors = [
+  "#1b191f",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+  "#CBD62D",
+  "#d62dcb"
+];
+
+const refs = {
+  body: document.querySelector("body"),
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]')
+};
+
+const colorChangeTimer = {
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.colorId = setInterval(() => {
+      const randomIndex = randomIntegerFromInterval(0, colors.length - 1);
+      refs.body.style.backgroundColor = colors[randomIndex];
+      console.log(randomIndex)
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.colorId);
+    this.isActive = false;
+    refs.body.style.backgroundColor = null;
+  }
+};
+
+refs.startBtn.addEventListener("click", colorChangeTimer.start.bind(colorChangeTimer));
+refs.stopBtn.addEventListener("click", colorChangeTimer.stop.bind(colorChangeTimer));
